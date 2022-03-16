325b3131fc7dc616598276781363354d46fa7685
choco lenka <timland26@gmail.com>
2022-03-16 16:37:13 +0300

    Initial commit

---
+++ src/com/company/Cycles.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Cycles {
+    public static void main(String[]args){
+    //Поднимаемся по ступенькам с 1-й по 9-ю
+
+    for (int i=1;i<=9;i++)
+        System.out.println(i);
+    for (int i=1;i<=9;i++)
+        System.out.println(i);
+}
+}
